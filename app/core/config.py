fc8b17a9899c1c9e59238262cb1fcec1530b2565
Alexey Filichkin <filichkin.alexey@gmail.com>
2025-08-10 21:40:45 +0300

    created config.py and db.py

---
+++ app/core/config.py
@@ -0,0 +1,19 @@
+from typing import Optional
+
+from pydantic import EmailStr
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    app_title: str = 'Приложение QRKot'
+    description: str
+    database_url: str
+    secret: str = 'SECRET'
+    first_superuser_email: Optional[EmailStr] = None
+    first_superuser_password: Optional[str] = None
+
+    class Config:
+        env_file = '.env'
+
+
+settings = Settings()
