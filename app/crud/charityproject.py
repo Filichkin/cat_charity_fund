fb26884d6cf71e60d3a2def7e71d2ee52f015d04
Alexey Filichkin <filichkin.alexey@gmail.com>
2025-08-11 13:47:39 +0300

    created crud options

---
+++ app/crud/charityproject.py
@@ -0,0 +1,19 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.crud.base import CRUDBase
+from app.models.charityproject import CharityProject
+
+
+class CRUDCharityProject(CRUDBase):
+
+    async def get_not_fully_invested(self, session: AsyncSession):
+        db_objs = await session.execute(
+            select(CharityProject)
+            .where(CharityProject.fully_invested.is_(False))
+            .order_by(CharityProject.create_date)
+        )
+        return db_objs.scalars().all()
+
+
+charity_project_crud = CRUDCharityProject(CharityProject)
